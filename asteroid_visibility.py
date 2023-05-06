c555a423ae22637dfbf8de11b041261c748fba74
rszakats <rszakats@konkoly.hu>
2023-05-06 13:09:19 +0200

    Added new search function, dataread function, replaced bars with rectangles.

--- asteroid_visibility.py
+++ asteroid_visibility.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python3.8
 
 import numpy as np
-import matplotlib.pyplot as plt
-from astropy.visualization import astropy_mpl_style, quantity_support
 import astropy.units as u
 from astropy.time import Time
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
 from astroquery.jplhorizons import Horizons
-# import datetime
 from astropy.coordinates import get_sun
 from astropy.coordinates import get_moon
 from astropy.table import Table
-# import time
 import getpass
 import os
 import warnings
 from bokeh.plotting import Figure, figure
-from bokeh.models import ColumnDataSource, WheelZoomTool, DatePicker, TextInput, ResetTool, SaveTool, PanTool, Select, AutocompleteInput, MultiLine, Select
+from bokeh.models import ColumnDataSource, DatePicker, TextInput, Select, AutocompleteInput, Select
 from bokeh.io import curdoc
 from bokeh.layouts import column, row
 from bokeh.palettes import d3, Viridis256
@@ -26,10 +22,8 @@ from timezonefinder import TimezoneFinder
 import pytz
 from pytz import timezone
 from datetime import datetime
-# from bokeh.models.glyphs import VBar
 from astname import get_indexfile
 import platform
-import copy
 
 warnings.filterwarnings("ignore")
 
@@ -49,9 +43,11 @@ def download_asteroid_data():
     inputfile = f"{cachedir}{datafile}"
     return inputfile
 
-
+#-----------------------------------------------------------------------------------------------------------
 def calc_visibility(name, otime, observing_location, observatoryid):
-    fmt = '%Y-%m-%d %H:%M:%S'
+    # fmt = '%Y-%m-%d %H:%M:%S'
+    otime = str(otime).split()[0]
+    # print(f"otime: {otime}")
     utc = pytz.utc
     loc = TimezoneFinder()
     timez = loc.timezone_at(lng=observing_location.lon.value, lat=observing_location.lat.value)
@@ -67,14 +63,14 @@ def calc_visibility(name, otime, observing_location, observatoryid):
     
     jd = Time(otime - 2*utcoffset*u.hour).jd
     print(f"Processing: {name} ")
-    obj = Horizons(id=name, location=observatoryid, epochs=jd)
+    obj = Horizons(id=f"{name}:", location=observatoryid, epochs=jd)
     target = obj.ephemerides()
     center = SkyCoord(target['RA'][0]*u.deg, target['DEC'][0]*u.deg,
                         frame='icrs')
     center = center.transform_to(AltAz(obstime=otime,
                                     location=observing_location))
 
-    print(f"utcoffset (h): {utcoffset}")
+    # print(f"utcoffset (h): {utcoffset}")
     midnight = otime - 2*utcoffset*u.hour
     delta_midnight = np.linspace(-10, 10, 1000)*u.hour
     times = midnight + delta_midnight + utcoffset*u.hour
@@ -91,7 +87,7 @@ def calc_visibility(name, otime, observing_location, observatoryid):
     label2 = []
     label3 = []
     color3 = []
-    for i in range(len(delta_midnight)):
+    for _ in range(len(delta_midnight)):
         label1.append("Sun")
         label2.append("Moon")
         label3.append(name)
@@ -105,20 +101,22 @@ def calc_visibility(name, otime, observing_location, observatoryid):
                    'ys': moonaltazs.alt.value,
                    'labels': label2,
                    'time': times.value}
-    source3.data = {'xs':delta_midnight.value,
-                   'ys': center2.alt.value,
+    source3.data = {'xs':delta_midnight.value.tolist(),
+                   'ys': center2.alt.value.tolist(),
                    'labels': label3,
                    'color': color3,
-                   'az': center2.az.value,
-                   'time': times.value}
+                   'az': center2.az.value.tolist(),
+                   'time': times.value.tolist()}
+
     interval4 = source1.data['xs'][sunaltazs.alt < -0*u.deg]
     center4 = interval4[int(len(interval4)/2)]
     interval5 = source1.data['xs'][sunaltazs.alt < -12*u.deg]
     center5 = interval5[int(len(interval5)/2)]
     source4.data = {'center': [center4], 'width': [max(source1.data['xs'][sunaltazs.alt < -0*u.deg])-min(source1.data['xs'][sunaltazs.alt < -0*u.deg])]}
     source5.data = {'center': [center5], 'width': [max(source1.data['xs'][sunaltazs.alt < -12*u.deg])-min(source1.data['xs'][sunaltazs.alt < -12*u.deg])]}
+    # print(f"source3.data:\n{source3.data}")
 
-
+#-----------------------------------------------------------------------------------------------------------
 def callback(attr, old, new):
     if sname.value == "":
         sname.value = '1'
@@ -126,132 +124,165 @@ def callback(attr, old, new):
         date.value = "2023-05-05"
     if autoc.value == "":
         autoc.value = "Piszkesteto Stn. (Konkoly)"
-    print(f"sname.value: {sname.value}")
-    orig = copy.copy(sname.value)
     sname.value = str(sname.value).split()[0]
-    print(f"sname.value after split: {sname.value}")
     obsdata = get_obsdata(observatories, autoc.value)
-    print(f"obsdata:\n{obsdata}")
     observing_location = EarthLocation(lat=obsdata['gclat'][0], lon=obsdata['long'][0],
                                    height=obsdata['height'][0]*u.m)
     calc_visibility(sname.value, date.value, observing_location, obsdata['code'][0])
-    p.title.text = f"Visibility plot for {orig} at {date.value} from {autoc.value}"
+    update_info()
 
-def search_callback(attr, old, new):
-    # get the search term from the input widget
-    term = new.lower()
-    print(f"term: {term}")
+#-----------------------------------------------------------------------------------------------------------
+def search_callback2(attr, old, new):
+    try:
+        targ = int(new)
+        if isinstance(targ, int):
+            if int(targ) > 2000000 and int(targ) < 50000000:
+                result = fulldata[np.where(fulldata['naifid'] == f'{new}')]
+                sname.options = [i +" "+ j  for i, j in zip(result['astnum'].tolist(), result['astid'].tolist())]
+                source0.data = {'ssos': sname.options}
+            else:
+                result = fulldata[np.where(fulldata['astnum'] == f'{new}')]
+                sname.options = [i +" "+ j  for i, j in zip(result['astnum'].tolist(), result['astid'].tolist())]
+                source0.data = {'ssos': sname.options}
+    except ValueError:
+        if isinstance(new, str):
+            result = fulldata[np.core.defchararray.find(np.char.lower(fulldata['astid'].value), f"{new}".lower())!=-1]
+            sname.options = [i +" "+ j  for i, j in zip(result['astnum'].tolist(), result['astid'].tolist())]
+            source0.data = {'ssos': sname.options}
+    if len(sname.options) > 0:
+        sname.value = sname.options[0]
+        update_info()
 
-    # filter the data based on the search term
-    filtered_data = {'ssos': []}
-    for i in range(len(data['ssos'])):
-        row = {'ssos': data['ssos'][i]}
+#-----------------------------------------------------------------------------------------------------------
+def update_info():
+    data = fulldata[np.where(fulldata['astnum'] == f'{sname.value}')]
+    info = f"Number: {data['astnum'][0]}\nName/ID: {data['astid'][0]}\n"\
+           f"NAIFID: {data['naifid'][0]}\nOther ID(s): {str(data['altid'][0]).replace(',', ';')}"
+    infos = []
+    labels = []
+    for i in range(len(source3.data['xs'])):
+        labels.append(f"{data['astnum'][0]} {data['astid'][0]}")
+        infos.append(info)
+    source3.data['info'] = infos
+    source3.data['labels'] = labels
+    p.title.text = f"Visibility plot for {data['astnum'][0]} {data['astid'][0]} at {date.value} from {autoc.value}"
 
-        # loop over each value in the row
-        for value in row.values():
-            value_str = str(value).lower()
-
-            # check if the value contains the search term
-            if term in value_str:
-                filtered_data['ssos'].append(row['ssos'])
-                print(f"Match! {term} {value_str}")
-                break
-
-    # update the data source with the filtered data
-    # source.data = filtered_data
-    # print(f"source.data:\n{source.data}")
-    # print(f"search.value: {search.value}")
-    source0.data = {'ssos': filtered_data['ssos']}
-    sname.options = filtered_data['ssos']
-    # sname.value = filtered_data['ssos'][0]
-
-# def create_renderers(colormap, name):
-#     xs = [source3.data['xs']]
-#     ys = [source3.data['ys']]
-#     l = source3.data['az']
-
-#     color_mapper = LinearColorMapper(palette=colormap, 
-#         low=min(l), high=max(l))
-
-#     glyph = MultiLine(xs="xs", ys="ys", 
-#                       line_color={'field': 'az', 'transform': color_mapper}, 
-#                       line_width=2, name=name)
-
-#     color_bar = ColorBar(color_mapper=color_mapper, title=name)
-#     return glyph, color_bar
-
-
+    
+#-----------------------------------------------------------------------------------------------------------
 def read_obsdata():
     # https://dc.zah.uni-heidelberg.de/obscode/q/query/form
     t = Table.read('observatories.csv', format='ascii.csv')
     return t
 
+#-----------------------------------------------------------------------------------------------------------
 def get_obsdata(observatories, observatory):
     obs = observatories[observatories['name'] == observatory]
     return obs
 
-def conv(s):
-    return s.strip()
+#-----------------------------------------------------------------------------------------------------------
+# def conv(s):
+#     return s.strip()
 
+#-----------------------------------------------------------------------------------------------------------
+def read_astdata(infile):
+    astnums = []
+    astids = []
+    naifids = []
+    altids = []
+    i = 0
+    with open(infile, "r") as file:
+        for line in file:
+            data = line.split(",")
+            astnums.append(data[0].split()[0])
+            if len(data[0].split()[1:]) > 1:
+                ids = ""
+                for ii in data[0].split()[1:]:
+                    ids += f"{ii} "
+                astids.append(ids)
+            else:
+                astids.append(data[0].split()[1:][0])
+            naifids.append(data[1])
+            alt = ""
+            for j in range(2, 8):
+                try:
+                    alt += str(line.split(',')[j])+", "
+                except IndexError:
+                    alt = alt
+            alt = alt.replace(", ,", "")
+            alt = alt.rstrip(" ,\n")
+            altids.append(alt)
+            i += 1
+    t = Table([astnums, astids, naifids, altids],
+            names=("astnum", "astid", "naifid", "altid"),meta={'name': 'first table'})
+    return t
+
+
+#-----------------------------------------------------------------------------------------------------------
 astdat_file = download_asteroid_data()
+fulldata = read_astdata(astdat_file)
+astdat = [i +" "+ j  for i, j in zip(fulldata['astnum'].tolist(), fulldata['astid'].tolist())]
 
-astdat = np.loadtxt(astdat_file, delimiter=",", dtype=str, usecols=0, converters={0: conv}).tolist()
-# print(f"astdat: {astdat[0:10]}")
 data = {'ssos': astdat}
 source = ColumnDataSource(data)
 
 user = getpass.getuser()
 if os.path.exists("/tmp/"+str(user)) is False:
     os.mkdir("/tmp/"+str(user))
-plt.style.use(astropy_mpl_style)
-quantity_support()
-hover = HoverTool(line_policy='interp',
-                  tooltips=[("object", "@labels"), ("UTC", "@time"), ("delta Time from midnight", "@xs"), ("altitude", "@ys"), ("azimuth", "@az")],
-                  point_policy='snap_to_data')
-
-p = figure(height=1000, width=1200, y_range=(0, 90) , x_range=(-10, 10))#, tools=[hover]) #  , x_range=(-12, 12)
 
 observatories = read_obsdata()
 
-source0 = ColumnDataSource(data={'ssos':['1']})
+source0 = ColumnDataSource(data={'ssos':['1 Ceres']})
 source1 = ColumnDataSource(data={'xs':[], 'ys': [], 'labels': []})
 source2 = ColumnDataSource(data={'xs':[], 'ys': [], 'labels': []})
-source3 = ColumnDataSource(data={'xs':[], 'ys': [], 'labels': ['1'], 'color': [], 'az': [1.], 'time': [datetime(2023, 5, 5), datetime(2023, 5, 6)]})
+source3 = ColumnDataSource(data={'xs':[], 'ys': [], 'labels': ['1'],
+                                 'color': [], 'az': [1.],
+                                 'time': [datetime.now(), datetime.now()],
+                                 'info': []})
 source4 = ColumnDataSource(data={'center': [0], 'width': [0]})
 source5 = ColumnDataSource(data={'center': [0], 'width': [0]})
 
-date = DatePicker(title='Choose date', value="2023-05-05")
-name = TextInput(title='Write target name')
-# sname = AutocompleteInput(title='Search Target:', completions=astdat, case_sensitive=False, placeholder="Search for an asteroid.")
+date = DatePicker(title='Choose date', value=f"{datetime.now()}")
+name = TextInput(title='Search for target')
 sname = Select(title='Choose a target:', options=source0.data['ssos'], value=f"{source0.data['ssos'][0]}")
-autoc = AutocompleteInput(title='Search Observatory:', completions=observatories['name'].tolist(), case_sensitive=False)
+autoc = AutocompleteInput(title='Search Observatory:', completions=observatories['name'].tolist(),
+                          case_sensitive=False, placeholder="Search for an observatory.")
 
 date.on_change('value', callback)
-name.on_change('value', search_callback)
+name.on_change('value', search_callback2)
+name.on_change('value', callback)
 sname.on_change('value', callback)
 autoc.on_change('value', callback)
+
+p = figure(height=1000, width=1200, y_range=(0, 90) , x_range=(-10, 10))#, tools=[hover]) #  , x_range=(-12, 12)
+p.rect(x='center', y=45, width='width', height=90, source=source5,
+       fill_alpha=0.8, line_color=d3['Category20c'][20][-1], fill_color=d3['Category20c'][20][-1])
+p.rect(x='center', y=45, width='width', height=90, source=source4,
+       fill_alpha=0.4, line_color=d3['Category20c'][20][16], fill_color=d3['Category20c'][20][16])
+
+color_mapper = LinearColorMapper(palette=Viridis256, low=0, high=360)
+sun = p.line(x='xs', y='ys', source=source1, legend_label="Sun", color="orange", line_width=1)
+moon = p.line(x='xs', y='ys', source=source2, legend_label="Moon", color=d3['Category10'][3][0], line_width=2, line_dash='dashed')
+targ = p.circle(x='xs', y='ys', source=source3, legend_field="labels", color={'field': 'az', 'transform': color_mapper})
+hline = Span(location=20, dimension='width', line_color='red', line_width=2, line_dash='dashed', line_alpha=0.5)
+p.renderers.extend([hline])
+
+bar = ColorBar(color_mapper=color_mapper, location=(0,0), title="Azimuth (deg)")
+p.add_layout(bar, "right")
+
+p.add_tools(HoverTool(renderers=[sun, moon, targ],line_policy='interp',
+                  tooltips=[("object", "@labels"), ("UTC", "@time"), ("delta Time from midnight", "@xs"),
+                            ("altitude", "@ys"), ("azimuth", "@az"),
+                            ("Info:" ,"@info")],
+                  point_policy='snap_to_data'))
+
+p.xaxis.axis_label = 'Hours from Local Midnight'
+p.yaxis.axis_label = 'Altitude (deg)'
+p.legend.click_policy="hide"
+p.title.text = f"Visibility plot for {str(source3.data['labels'][0])} at {date.value} from {autoc.value}"
 
 top_row = row(date, name, sname, autoc)
 middle_row = row(p)
 
-p.vbar(source=source5, x='center' ,width='width', bottom=0,top=90, color=d3['Category20c'][20][-1], fill_alpha=0.8, line_color=None)
-p.vbar(source=source4, x='center' ,width='width', bottom=0,top=90, color=d3['Category20c'][20][16], fill_alpha=0.4, line_color=None)
-
-color_mapper = LinearColorMapper(palette=Viridis256, low=0, high=360)
-p.line(x='xs', y='ys', source=source1, legend_label="Sun", color="orange", line_width=1)
-p.line(x='xs', y='ys', source=source2, legend_label="Moon", color=d3['Category20c'][4][1], line_width=2, line_dash='dashed')
-p.circle(x='xs', y='ys', source=source3, legend_field="labels", color={'field': 'az', 'transform': color_mapper})
-hline = Span(location=20, dimension='width', line_color='red', line_width=2, line_dash='dashed', line_alpha=0.5)
-p.renderers.extend([hline])
-p.add_tools(HoverTool(line_policy='interp',
-                  tooltips=[("object", "@labels"), ("UTC", "@time"), ("delta Time from midnight", "@xs"), ("altitude", "@ys"), ("azimuth", "@az")],
-                  point_policy='snap_to_data'))
-p.xaxis.axis_label = 'Hours from Local Midnight'
-p.yaxis.axis_label = 'Altitude (deg)'
-p.legend.click_policy="hide"
-bar = ColorBar(color_mapper=color_mapper, location=(0,0), title="Azimuth (deg)")
-p.add_layout(bar, "right")
-p.title.text = f"Visibility plot for {str(source3.data['labels'][0])} at {date.value} from {autoc.value}"
 curdoc().add_root(column(top_row, middle_row))
 curdoc().theme = 'caliber'
 curdoc().title = "Visibility plot"
